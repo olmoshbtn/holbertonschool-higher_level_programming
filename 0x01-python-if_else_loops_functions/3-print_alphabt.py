b2364a5a0b62bb7e918b3af1c647eaa5bfe46652
olmoshbtn <2120@holbertonschool.com>
2020-12-08 19:25:43 +0000

    3.alphabet without q and e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-import string
-for alpha_letters in string.ascii_lowercase:
-    if alpha_letters not in 'qe':
-       print(alpha_letters, end="")
+for letter in range(97, 123):
+    if letter != 101 and letter != 113:
+        print('{}'.format(chr(letter)), end='')
