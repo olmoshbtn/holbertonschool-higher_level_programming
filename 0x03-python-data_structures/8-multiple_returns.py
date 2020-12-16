06f241a5a9b69b19f0923a58ee9c0035284c2125
olmoshbtn <2120@holbertonschool.com>
2020-12-16 01:59:33 +0000

    8. More returns!

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    
+
     len_sen = len(sentence)
+
     if len_sen == 0:
-        return None, None 
+        return None, None
+
     return (len_sen, sentence[0])
