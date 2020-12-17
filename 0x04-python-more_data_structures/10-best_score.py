ae130346673bd4504c9288dda94d8f83da2384fc
olmoshbtn <2120@holbertonschool.com>
2020-12-17 23:57:51 +0000

    10. Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if not a_dictionary:
         return None
-    max_key = max(a_dictionary, key=a_dictionary.get)
+    else:
+        max_key = max(a_dictionary, key=a_dictionary.get)
     return max_key
