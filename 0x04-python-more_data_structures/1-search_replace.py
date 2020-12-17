7a546e5023afb8dfa9a0721db708ebcc16832c84
olmoshbtn <2120@holbertonschool.com>
2020-12-17 19:44:20 +0000

    1. Search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,4 @@
 #!+/usr/bin/python3
 def search_replace(my_list, search, replace):
-    return [replace if search is value else value for value in my_list]
+    new_list = [replace if search is value else value for value in my_list]
+    return new_list
