02f727fee0fb2357f2223863dd42ee2fcc4eb1ab
olmoshbtn <2120@holbertonschool.com>
2020-12-18 23:53:32 +0000

    12. Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,7 +5,7 @@ def roman_to_int(roman_string):
     dict_roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
                   'C': 100, 'D': 500, 'M': 1000}
     result = 0
-    
+
     for i in range(len(roman_string) - 1):
         actual = dict_roman[roman_string[i]]
         after = dict_roman[roman_string[i + 1]]
@@ -13,5 +13,5 @@ def roman_to_int(roman_string):
             result -= actual
         else:
             result += actual
-    result += dict_roman[roman_string[len(roman_string) - 1]] 
+    result += dict_roman[roman_string[len(roman_string) - 1]]
     return result
