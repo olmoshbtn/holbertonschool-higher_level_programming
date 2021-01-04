9c08019995d6aff7ee39d8e4def648ad5e9af300
olmoshbtn <2120@holbertonschool.com>
2021-01-04 20:31:59 +0000

    7:27: E225 missing whitespace around operator

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
     for index in range(x):
         try:
             print("{:d}".format(my_list[index]), end="")
-            int_printed +=1
+            int_printed += 1
         except (ValueError, TypeError):
             pass
     print()
