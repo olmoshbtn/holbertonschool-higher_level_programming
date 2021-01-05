08523e7a147b0e2e1d3919100fb60cc303b16e39
olmoshbtn <2120@holbertonschool.com>
2021-01-05 01:04:41 +0000

    100-safe_print_integer_err.py

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as error:
+        print("Exception: {}".format(error), file=__import__('sys').stderr)
+        return False
