36084aa10b0abb0db451fa3c776e2c03869bf596
olmoshbtn <2120@holbertonschool.com>
2021-01-05 22:22:29 +0000

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+"""Empty class that defines a Square
+"""
 
 
 class Square:
-    """Empty definition of a 'Square'
+    """Empty definition of a Square
     """
     pass
