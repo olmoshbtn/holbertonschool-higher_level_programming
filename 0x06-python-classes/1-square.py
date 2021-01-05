e303657a5012da14024cc17131bcfd0a2459792a
olmoshbtn <2120@holbertonschool.com>
2021-01-05 22:33:49 +0000

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""Class named Square with a private instace attribute
+"""
+
+
+class Square:
+    """Definition of a Square
+    """
+    def __init__(self, size):
+        """Instantiate a Square
+        """
+        self.__size = size
