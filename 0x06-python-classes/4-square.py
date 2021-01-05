66d73385b67c139f54220c9efee5f4b0164dbe24
olmoshbtn <2120@holbertonschool.com>
2021-01-05 23:04:43 +0000

    4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""Class Square that defines a square
+"""
+
+
+class Square:
+    """Definition of a Square
+    """
+    def __init__(self, size=0):
+        """Instantiate a Square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        """
+        self.__size = size
+
+    @size.setter
+    def size(self, size):
+        """
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """ Current square area
+        """
+        return self.__size ** 2
