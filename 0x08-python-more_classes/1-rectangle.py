3949fc44e3e5a2fdca049d851f0ded37c72a9f3b
olmoshbtn <2120@holbertonschool.com>
2021-01-11 22:17:37 +0000

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,8 +6,8 @@ class Rectangle:
     """Definition to represent the 'Rectangle' Class"""
     def __init__(self, width=0, height=0):
         """Instantiate a rectangle"""
-        self.width = width
         self.height = height
+        self.width = width
 
     @property
     def width(self):
