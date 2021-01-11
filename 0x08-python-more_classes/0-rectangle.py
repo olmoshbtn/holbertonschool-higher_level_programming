27fcc5b53a147470ae0129241ebaa762e2c61e99
olmoshbtn <2120@holbertonschool.com>
2021-01-11 20:58:21 +0000

    0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""Module that define an empty 'Rectangle' Class to represent a rectangle"""
+"""Module that define an empty 'Rectangle' Class to represent a rectangle
+"""
 
 
 class Rectangle:
-    """Empty definition to represent the 'rectangle' class"""
+    """Empty definition to represent the 'rectangle' class
+    """
