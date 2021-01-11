adf7ad9398c714f54c92fe1d4673dff19cd19aa1
olmoshbtn <2120@holbertonschool.com>
2021-01-11 00:42:56 +0000

    0-add_integer.py

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module provides a
+function 'add_integer'
+that performs
+integer
+addition"""
+
+
+def add_integer(a, b=98):
+    """Function that 
+    return the sum of two 
+    integers or floats"""
+    if not isinstance(a, (float, int)):
+        raise TypeError("a must be an integer")
+
+    if not isinstance(b, (float, int)):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
