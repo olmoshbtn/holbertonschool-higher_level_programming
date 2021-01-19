6eb09df0c96f0b9461f686768e6efcff7853f41f
olmoshbtn <2120@holbertonschool.com>
2021-01-19 23:13:47 +0000

    8-rectangle.py

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module with BaseGeometry class"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """"Representation of a rectangule"""
+    def __init__(self, width, height):
+        """Instantiation of a rectangle"""
+        self.integer_validator("with", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
