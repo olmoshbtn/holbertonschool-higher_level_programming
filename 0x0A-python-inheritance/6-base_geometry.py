7a3074bae133e790351bcd2e3dbad6e103b02e4c
olmoshbtn <2120@holbertonschool.com>
2021-01-19 23:12:28 +0000

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module with BaseGeometry class"""
+
+
+class BaseGeometry():
+    """Class with atribute public area"""
+    pass
+    """Raise an exception when is called"""
+    def area(self):
+        raise Exception("area() is not implemented")
