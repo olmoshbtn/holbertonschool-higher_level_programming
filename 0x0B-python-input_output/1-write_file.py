87ac29afae2f3c6c6820849d09c04d2f22596ebf
olmoshbtn <2120@holbertonschool.com>
2021-01-20 22:59:24 +0000

    1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,5 +4,5 @@
 
 def write_file(filename="", text=""):
     """Writes a string text file (UTF8) and returns number of char written"""
-    with open(filename, "r", encoding="utf-8") as w_f:
+    with open(filename, "w", encoding="utf-8") as w_f:
         return w_f.write(text)
