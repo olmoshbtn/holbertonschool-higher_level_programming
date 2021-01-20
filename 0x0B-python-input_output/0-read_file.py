225c01b4f2a6a4a079df5c0c05ff5edad53e9bd6
olmoshbtn <2120@holbertonschool.com>
2021-01-20 20:30:33 +0000

    0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module function read_file"""
+
+
+def read_file(filename=""):
+    """Read a text file (UTF-8) and prints it in stdout"""
+    with open(filename, mode='r', encoding='UTF-8') as seb:
+        print(seb_pep8.read(), end='')
