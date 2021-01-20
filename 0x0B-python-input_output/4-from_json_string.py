f487adac4833ac7a1cb6f94bd1f5a36ecb8c5534
olmoshbtn <2120@holbertonschool.com>
2021-01-20 23:53:29 +0000

    4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,9 +1,9 @@
 #!/usr/bin/pyhton3
 """Module function from_json_string"""
-
 import json
 
 
 def from_json_string(my_str):
-    """returns an object represented by a JSON string"""
+    """returns an object (Python data structure)
+    represented by a JSON string"""
     return json.loads(my_str)
