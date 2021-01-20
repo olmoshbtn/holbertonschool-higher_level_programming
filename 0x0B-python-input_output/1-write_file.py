7bf48f4359bdb9c660784d54ede39b56e3dea0e9
olmoshbtn <2120@holbertonschool.com>
2021-01-20 22:52:01 +0000

    1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module function write_file"""
+
+
+def write_file(filename="", text=""):
+    """Writes a string text file (UTF8) and returns number of char written"""
+    with open(filename, "r", encoding="utf-8") as w_f:
+        return w_f.write(text)
