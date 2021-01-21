50bf5181f0b9bccaee22d58dd1cf527b26ed4997
olmoshbtn <2120@holbertonschool.com>
2021-01-21 02:08:13 +0000

    10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -3,17 +3,21 @@
 
 
 class Student:
-    """Definition of a student"""
+    """Definition of student"""
     def __init__(self, first_name, last_name, age):
-        """Instantiate a student"""
+        """Initializes of student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Get a JSON representation of a student"""
-        if isinstance(attrs, list) and all(
-                map(lambda s: isinstance(s, str), attrs)
-        ):
-            return {k: self.__dict__[k] for k in attrs if k in self.__dict__}
-        return self.__dict__
+        """retrieves a dictionary representation of a Student instance"""
+        if attrs is None:
+            return self.__dict__
+        new_dict = {}
+        for a in attrs:
+            try:
+                new_dict[a] = self.__dict__[a]
+            except:
+                pass
+        return new_dict
