bb5a5045e3eb8e53c0f4e66882c300fcfb70c668
olmoshbtn <2120@holbertonschool.com>
2021-03-02 23:43:24 +0000

    2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- create database hbtn_0d_2
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2'
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
 -- create user user_0d_2
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 -- grant read-only privilleges to user_0d_2
-GRANT SELECT ON 'user_0d_2'.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
