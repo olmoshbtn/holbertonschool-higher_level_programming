69b309d8bca09fd20030067180cd53476d3f14f6
olmoshbtn <2120@holbertonschool.com>
2021-03-03 00:00:02 +0000

    6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- create the database hbtn_0d_usa
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 -- create the table states
-CREATE TABLE IF NOT EXISTS `hbtn_0d_usa.states` (
+CREATE TABLE IF NOT EXISTS `states` (
     `id` INT AUTO_INCREMENT NOT NULL,
     `name` VARCHAR(256) NOT NULL,
     PRIMARY KEY (`id`)
