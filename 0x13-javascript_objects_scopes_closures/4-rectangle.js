ba99a3b207d5fad55fa9950d45726dedd407d85d
olmoshbtn <2120@holbertonschool.com>
2021-03-23 23:25:30 +0000

    4-rectangle.js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -6,16 +6,18 @@ module.exports = class Rectangle {
       this.height = h;
     }
   }
+
   print () {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
   }
+
   rotate () {
-    let tmp = this.width;
     this.width = this.height;
-    this.height = tmp;
+    this.height = this.width;
   }
+
   double () {
     this.width *= 2;
     this.height *= 2;
