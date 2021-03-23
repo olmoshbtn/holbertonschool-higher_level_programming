f45a3abfc1cea739953462df1b3fd442ed5f8502
olmoshbtn <2120@holbertonschool.com>
2021-03-23 12:28:34 +0000

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
-for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
-  console.log(line);
+const lines = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < lines.length; i += 1) {
+  console.log(lines[i]);
 }
