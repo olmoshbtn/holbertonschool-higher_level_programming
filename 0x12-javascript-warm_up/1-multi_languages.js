8f131dda0c453de5eb6ea0d2ab4fdfc62b4473ed
olmoshbtn <2120@holbertonschool.com>
2021-03-23 11:45:22 +0000

    1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const lines = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (let i = 0; i < lines.length; i += 1) {
-	console.log(lines[i]);
+  console.log(lines[i]);
 }
