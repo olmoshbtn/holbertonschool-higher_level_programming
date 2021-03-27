6a4b07e6aabd34577799481c9768d33fd6e8d987
olmoshbtn <2120@holbertonschool.com>
2021-03-27 20:52:46 +0000

    4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -18,8 +18,8 @@ if __name__ == '__main__':
             )
         cursor = db.cursor()
         query = """
-            SELECT cities.id, cities.name, states.name 
-            FROM cities 
+            SELECT cities.id, cities.name, states.name
+            FROM cities
             JOIN states ON cities.state_id = states.id
             ORDER BY cities.id
             """
