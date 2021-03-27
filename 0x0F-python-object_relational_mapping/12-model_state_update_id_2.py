8e43f908a0f46c401dd6ea28e839186c2a6adf82
olmoshbtn <2120@holbertonschool.com>
2021-03-27 23:08:30 +0000

    12-model_state_update_id_2.py

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+ Prints the State object with the name passed as argument from
+ the database hbtn_0e_6_usa
+ """
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+
+if __name__ == '__main__':
+    try:
+        engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                argv[1], argv[2], argv[3]), pool_pre_ping=True
+                )
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        match = session.query(State).filter(State.id == 2).first()
+
+        if match is not None:
+            match.name = 'New Mexico'
+            session.commit()
+        session.close()
+
+    except Exception as e:
+        print("Error: {}".format(e))
