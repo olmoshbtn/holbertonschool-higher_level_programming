d79571b4db0c20d0536b50c58c98df57b7d97e67
olmoshbtn <2120@holbertonschool.com>
2021-03-27 23:15:30 +0000

    13-model_state_delete_a.py

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+ Changes the name of a State object from the database hbtn_0e_6_usa
+ """
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+
+if __name__ == '__main__':
+    try:
+        engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                argv[1], argv[2], argv[3]), pool_pre_ping=True
+                )
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        results = session.query(State).filter(State.name.contains('a')).all()
+    
+        for state in results:
+            session.delete(state)
+        session.commit()
+        session.close()
+
+    except Exception as e:
+        print("Error: {}".format(e))
