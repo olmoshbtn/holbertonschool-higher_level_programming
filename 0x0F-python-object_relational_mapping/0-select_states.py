a5d9e878c228652bdef89d5408bb6f0459925d60
olmoshbtn <2120@holbertonschool.com>
2021-03-27 16:03:01 +0000

    0-select_states.py

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Lists all states from the database hbtn_0e_0_usa
+"""
+
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            charset="utf8"
+            )
+        cur = db.cursor()
+        cur.execute("SELECT * FROM states ORDER BY id ASC")
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+        cur.close()
+        db.close()
+    except Exception as e:
+        print("Error: {}".format(e))
