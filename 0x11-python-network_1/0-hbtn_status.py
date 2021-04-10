f5f9816227f6c79471110edb9ce8f87be5fd50f5
olmoshbtn <2120@holbertonschool.com>
2021-04-10 00:32:38 +0000

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,10 +3,15 @@
 
 
 if __name__ == "__main__":
-    import urllib.request as request
-    with request.urlopen('https://intranet.hbtn.io/status') as r:
-        html = r.read()
-        print('Body response:')
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode('utf-8')))
+    import urllib.request
+    req = urllib.request.Request('https://intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        data = response.read()
+
+    data_type = type(data)
+    data_utf8 = data.decode('utf8')
+
+    print("Body response:")
+    print("\t- type: {}".format(data_type))
+    print("\t- content: {}".format(data))
+    print("\t- utf8 content: {}".format(data_utf8))
