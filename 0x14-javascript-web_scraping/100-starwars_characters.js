9e7059c24f2c27775551a3ddb4b28fa5005d8009
olmoshbtn <2120@holbertonschool.com>
2021-05-04 23:08:02 +0000

    100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,20 +1,33 @@
 #!/usr/bin/node
 // Print all characters of a Star Wars movie
-import request from 'request';
-const url = `https://swapi.co/api/films/${process.argv[2]}`;
-request(url, { json: true }, (err, resp, body) => {
-  if (err) {
-    console.log(err);
-  } else if (body) {
-    const characters = body.characters;
-    for (const i in characters) {
-      request(characters[i], { json: true }, (err, resp, body) => {
-        if (err) {
-          console.log(err);
-        } else if (body) {
-          console.log(body.name);
-        }
-      });
+const argv = process.argv;
+const urlFilm = 'https://swapi-api.hbtn.io/api/films/';
+const urlMovie = `${urlFilm}${argv[2]}/`;
+
+const request = require('request');
+
+request(urlMovie, function (error, response, body) {
+  if (error) {
+    console.error('error:', error);
+  } else {
+    try {
+      const rbody = JSON.parse(body);
+      for (const i of rbody.characters) {
+        request(i, function (errorc, responsec, bodyc) {
+          if (error) {
+            console.error('error:', errorc);
+          } else {
+            try {
+              const cbody = JSON.parse(bodyc);
+              console.log(cbody.name);
+            } catch (error) {
+              console.error('error:', error);
+            }
+          }
+        });
+      }
+    } catch (err) {
+      console.error('error:', err);
     }
   }
 });
