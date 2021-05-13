1238c2301522d4b7f0dfc1e60ba447476a2e09bd
olmoshbtn <2120@holbertonschool.com>
2021-05-13 00:21:11 +0000

    7-script.js

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,11 @@
+$(function () {
+    $.ajax({
+      crossDomain: true,
+      url: 'https://swapi.co/api/people/5/?format=json',
+      success: function (data) {
+        const name = data.name;
+        $('DIV#character').text(name);
+      }
+    });
+  });
+  
