5b1f577f1f736a370aefd57c5c51cd2817bf8d68
olmoshbtn <2120@holbertonschool.com>
2021-05-13 00:17:35 +0000

    4-script.js

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,13 @@
+$(function () {
+    const header = $('header');
+  
+    $('DIV#toggle_header').click(function () {
+      header.toggleClass('red');
+      if (header.hasClass('red')) {
+        header.removeClass('green');
+      } else {
+        header.addClass('green');
+      }
+    });
+  });
+  
