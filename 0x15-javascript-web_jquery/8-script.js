951b1dc523c777fe26347ae166774714ee234d71
olmoshbtn <2120@holbertonschool.com>
2021-05-13 00:21:39 +0000

    8-script.js

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,13 @@
+$(function () {
+    $.ajax({
+      crossDomain: true,
+      url: 'https://swapi.co/api/films/?format=json',
+      success: function (data) {
+        const listFilms = data.results;
+        $.each(listFilms, function (index, value) {
+          $('UL#list_movies').append('<li>' + value.title + '</li>');
+        });
+      }
+    });
+  });
+  
