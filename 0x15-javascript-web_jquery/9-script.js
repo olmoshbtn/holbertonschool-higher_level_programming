e471984204ce056ab0b051d110f8cfc0e96f80b6
olmoshbtn <2120@holbertonschool.com>
2021-05-13 00:23:18 +0000

    9-script.js

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,11 @@
+$(function () {
+    $.ajax({
+      crossDomain: true,
+      url: 'https://fourtonfish.com/hellosalut/?lang=fr',
+      success: function (data) {
+        const hello = data.hello;
+        $('DIV#hello').text(hello);
+      }
+    });
+  });
+  
